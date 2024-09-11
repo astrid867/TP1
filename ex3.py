e45063a6d941228e69dbe5f041376ab9ec17c9f1
Rym Fortas <fortasrym12@gmail.com>
2024-09-11 19:40:18 -0400

    version1

--- ex3.py
+++ ex3.py
@@ -1,5 +1,8 @@
 # TODO: Calculer la distance qu'un poids peut atteindre en utilisant la formule de la portée d'un projectile.
 # TODO: Importer les modules nécessaires.
 
-speed = ... 
-angle = ...
+import math
+vitesse = input ("Quelle est la vitesse initiale de la balle?(en m/s)")
+angle = input ("quel est l'angle de lancement?(en degré)")
+distance = ((float(vitesse)**2)*(math.sin(2*(math.radians(float(angle))))))/9.81
+print("La distance maximale en x est :", distance, "m")
--- ex4.py
+++ ex4.py
@@ -3,4 +3,18 @@
 #        et affiche le résultat au format "XX km".
 #        Assurez une gestion du pourcentage valide au cours de votre programme (% toujours dans [0 ; 100]).
 
-battery_level = ...
+pourcentage_batterie = input("Quel est le pourcentage de la battrie du bateau?")
+if 50 < float(pourcentage_batterie) <= 100:
+    distance = (float(pourcentage_batterie) - 50) * 2 + (25*0.5) + (15*1) + (5*2.5)+(5*6)
+elif 25 < float(pourcentage_batterie) <= 50:
+    distance = (float(pourcentage_batterie) - 25) * 0.5 + (15*1) + (5*2.5)+(5*6)
+elif 10 < float(pourcentage_batterie) <= 25:
+    distance = (float(pourcentage_batterie) - 10) * 1 + (5*2.5)+(5*6)
+elif 5 <  float(pourcentage_batterie) <= 10:
+    distance =  (float(pourcentage_batterie) - 5) * 2.5 + (5*6)
+elif 0 <=  float(pourcentage_batterie) <= 5:
+    distance =  float(pourcentage_batterie) * 6
+else :
+    print (" La valeur entrée n'est pas valide.") 
+
+print("La distance possible avec",pourcentage_batterie, "%", "batterie est d'environ,",round(distance), "km.")
